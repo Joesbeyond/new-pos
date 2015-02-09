d06366d1ac45fb0a9cce547399aaf0607e7c744b
joesbeyond <joesbeyond@gmail.com>
2015-02-09 20:46:34 +0800

    delete tags

--- js/model/promotion/discountPromotion.js
+++ js/model/promotion/discountPromotion.js
@@ -4,24 +4,4 @@ function DiscountPromotion(type, barcodes) {
 
 DiscountPromotion.prototype = Object.create(Promotion.prototype);
 DiscountPromotion.prototype.constructor = DiscountPromotion;
-Promotion.loadDiscountPromotion = function() {
-    return [
-        new DiscountPromotion('SINGLE_DISCOUNT', [//95
-            'ITEM000000',
-            'ITEM000001',
-        ]),
-        new DiscountPromotion('BRAND_DISCOUNT', [//90
-            'ITEM000000',
-            'ITEM000010',
-        ]),
-        new DiscountPromotion('TOTAL_DISCOUNT', [//90
-            'ITEM000002',
-            'ITEM000003',
-            'ITEM000004',
-            'ITEM000005',
-            'ITEM000006',
-            'ITEM000007',
-            'ITEM000008'
-        ])
-    ];
-};
+
